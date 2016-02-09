60de47f569b8ac4f73438dcd6920e5a82b3c7dd2
Dmitry Smagin <exmortis@yandex.ru>
2016-02-09 11:18:21 +0300

    Commit sources 0.93.1 as-is

---
+++ src/sprite_eye.cc
@@ -0,0 +1,44 @@
+/**
+ * @file sprite_eye.cc 
+ * @brief The sprite of the eye used in bricks level 
+ * @created 2004-09-17
+ * @date 2007-09-12
+ * @copyright 1991-2014 TLK Games
+ * @author Bruno Ethvignot
+ * @version $Revision: 24 $
+ */
+/*
+ * copyright (c) 1991-2014 TLK Games all rights reserved
+ * $Id: sprite_eye.cc 24 2014-09-28 15:30:04Z bruno.ethvignot@gmail.com $
+ *
+ * TecnoballZ is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TecnoballZ is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+#include "../include/sprite_eye.h"
+
+/**
+ * Create the object
+ */
+sprite_eye::sprite_eye()
+{
+  clear_sprite_members();
+}
+
+/**
+ * Release the object
+ */
+sprite_eye::~sprite_eye()
+{
+}
