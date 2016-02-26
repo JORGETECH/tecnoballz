02afa6ad8ca3c4f08d5011d57d3697438bf54f29
Dmitry Smagin <exmortis@yandex.ru>
2016-02-26 09:58:51 +0300

    make_opk.sh: Include data dir without makefiles, add COPYING and README

--- make_opk.sh
+++ make_opk.sh
@@ -4,6 +4,11 @@ OPK_NAME=tecnoballz.opk
 
 echo ${OPK_NAME}
 
+# move src/data to tmp/data and remove makefiles
+mkdir tmp -p
+cp src/data tmp -r
+find tmp -type f -name 'Makefile*' -exec rm -f {} \;
+
 # create default.gcw0.desktop
 cat > default.gcw0.desktop <<EOF
 [Desktop Entry]
@@ -18,13 +23,16 @@ Categories=games;
 EOF
 
 # create opk
-FLIST="src/data"
+FLIST="tmp/data"
 FLIST="${FLIST} default.gcw0.desktop"
 FLIST="${FLIST} src/data/tecnoballz.png"
 FLIST="${FLIST} src/tecnoballz"
+FLIST="${FLIST} COPYING"
+FLIST="${FLIST} README"
 
 rm -f ${OPK_NAME}
 mksquashfs ${FLIST} ${OPK_NAME} -all-root -no-xattrs -noappend -no-exports
 
 cat default.gcw0.desktop
 rm -f default.gcw0.desktop
+rm -r tmp
